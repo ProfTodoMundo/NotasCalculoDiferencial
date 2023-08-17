ef82486c9d1a89a23b6fb03839ed2a0008701969
ProfTodoMundo <carlos.martinez@uacm.edu.mx>
2023-08-17 13:33:53 -0600

    qr_homework created

--- docs/GenQR.R
+++ docs/GenQR.R
@@ -3,9 +3,10 @@
 
 library(qrcode)
 setwd("~/Documents/GitHub/NotasCalculoDiferencial/docs")
+setwd("~/Desktop/MiGithub/NotasCalculoDiferencial/docs")
 # URL que deseas codificar en el QR
 url <- "https://proftodomundo.github.io/NotasCalculoDiferencial/"
-
+urltarea <- "https://github.com/ProfTodoMundo/NotasCalculoDiferencial/blob/PrincipalCD/docs/Tarea1.pdf"
 # Genera el código QR
 qr <- qr_code(url)
 
@@ -19,4 +20,21 @@ dev.off()
 png("codigo_qr.png", 
     width = 200, height = 200)
 plot(qr)
+dev.off()
+
+
+urltarea <- "https://github.com/ProfTodoMundo/NotasCalculoDiferencial/blob/PrincipalCD/docs/Tarea1.pdf"
+# Genera el código QR
+qrtarea <- qr_code(urltarea)
+
+# Guarda el código QR como una imagen PNG
+pdf("codigo_qr_tarea.pdf", 
+    width = 200, height = 200)
+plot(qrtarea)
+dev.off()
+
+
+png("codigo_qr_tarea.png", 
+    width = 200, height = 200)
+plot(qrtarea)
 dev.off()
